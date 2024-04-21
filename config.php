a5abbd590b9fe5f704f3c155ac12a1b7eb4c68b4
Saeed Ghorbani <161647727+saeed-54996@users.noreply.github.com>
2024-04-22 01:44:55 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'your_username',
+        'password' => 'your_password',
+        'dbname' => 'your_database_name'
+    ]
+];
